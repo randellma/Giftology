0bc7ba698b3c5184c7a27494e7b3d33f2359b17c
Matthew Randell <mrandell@Matthews-MacBook-Air.local>
2012-10-02 23:56:40 -0400

    Initial Commit

---
+++ Giftology/giftologyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  giftologyAppDelegate.h
+//  Giftology
+//
+//  Created by Matthew Randell on 2012-10-02.
+//  Copyright (c) 2012 Matthew Randell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface giftologyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GiftologyTests/GiftologyTests.m
@@ -0,0 +1,32 @@
+//
+//  GiftologyTests.m
+//  GiftologyTests
+//
+//  Created by Matthew Randell on 2012-10-02.
+//  Copyright (c) 2012 Matthew Randell. All rights reserved.
+//
+
+#import "GiftologyTests.h"
+
+@implementation GiftologyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GiftologyTests");
+}
+
+@end
